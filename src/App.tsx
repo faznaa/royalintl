8d2867c3ef47d165839c39842b9edf07197b6b78
FABI PATHIKKAL (ARTECH CONSULTING LLC) <v-fpathikkal@microsoft.com>
2024-08-19 08:28:53 -0700

    about added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
@@ -7,24 +9,30 @@ import ReactLenis from "lenis/dist/lenis-react";
 import GridBackground from "./components/GridBackground";
 import { TracingBeam } from "components/ui/TracingBeam";
 import Services from "components/Services";
+import About from "./pages/About";
+
 import { TracingBeam2 } from "components/ui/TracingBeam2";
 import AnimatedText2 from "components/AnimatedText2";
 
 function App() {
  
   return (
+    <Router>
     <div className="">
-      <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
-<link href="https://fonts.googleapis.com/css2?family=Plaster&display=swap" rel="stylesheet"/>
-      </head>
+    <Helmet>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link href="https://fonts.googleapis.com/css2?family=Plaster&display=swap" rel="stylesheet" />
+    </Helmet>
       <Navbar />
   
     <GridBackground />
-  
-      <Services />
+      <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,28 +1,25 @@
-// components/Navbar.js
-
+import { Link } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import MobileNavbar from "../MobileNavbar";
-// import { Link } from "react-router-dom";
+
 const NavbarItem = (props: any) => {
   const { isWhiteTheme, ..._props } = props;
   return (
-    <a
+    <Link
       {..._props}
       className="relative group py-2 my-5 mx-3 inline-block transition duration-300"
     >
       {_props.children}
       <div
-        className={`block  h-[1px]  w-full opacity-0 ${
+        className={`block h-[1px] w-full opacity-0 ${
           isWhiteTheme ? "bg-black" : "bg-white"
         } transition-opacity duration-500 group-hover:opacity-100 absolute left-0 bottom-0`}
       />
-    </a>
+    </Link>
   );
 };
 
 const Navbar = ({ isWhiteTheme = false }: any) => {
-  //   const { isAuthenticated } = useAuth()
-  //   const { setOpenLogin, setOpenRegister } = useAuthModalHook()
   const [hasScrolled, setHasScrolled] = useState(false);
 
   useEffect(() => {
@@ -31,19 +28,18 @@ const Navbar = ({ isWhiteTheme = false }: any) => {
       setHasScrolled(scrollTop > 0);
     };
 
-    // add scroll event listener when the component is mounted
     window.addEventListener("scroll", handleScroll);
 
-    // clean up the event listener when the component is unmounted
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+  
   const navigation = [
-    { href: "/", label: "Home" },
-    { href: "/about", label: "About" },
-    { href: "/services", label: "Services" },
-    { href: "/contact", label: "Contact" },
+    { to: "/", label: "Home" },
+    { to: "/about", label: "About" },
+    { to: "/services", label: "Services" },
+    { to: "/contact", label: "Contact" },
   ];
 
   return (
@@ -56,43 +52,33 @@ const Navbar = ({ isWhiteTheme = false }: any) => {
     >
       <MobileNavbar hasScrolled={hasScrolled || isWhiteTheme} />
 
-      <div className="hidden md:block  mx-auto lg:mx-16 px-4">
+      <div className="hidden md:block mx-auto lg:mx-16 px-4">
         <div className="flex justify-start items-center">
           <div className="flex justify-start items-center space-x-4">
-            <a href="/" className="pt-2 ">
+            <Link to="/" className="pt-2">
               {/* Website Logo */}
-              {hasScrolled || isWhiteTheme ? (
-                <img
-                  src="https://www.royalintl.in/wp-content/uploads/2022/03/royal-international-logo-2-2.png"
-                  width={100}
-                  height={100}
-                  alt="logo1"
-                  className="w-32"
-                />
-              ) : (
-                <img
-                  src="https://www.royalintl.in/wp-content/uploads/2022/03/royal-international-logo-2-2.png"
-                  width={100}
-                  height={100}
-                  alt="logo1"
-                  className="w-32"
-                />
-              )}
-            </a>
+              <img
+                src="https://www.royalintl.in/wp-content/uploads/2022/03/royal-international-logo-2-2.png"
+                width={100}
+                height={100}
+                alt="logo1"
+                className="w-32"
+              />
+            </Link>
           </div>
-         <div className="w-full flex justify-center mr-32">
-         <div className="hidden md:flex items-center justify-between space-x-1 gap-x-4 ">
-            {navigation.map((item) => (
-              <NavbarItem
-                key={item.href}
-                isWhiteTheme={isWhiteTheme}
-                href={item.href}
-              >
-                {item.label}
-              </NavbarItem>
-            ))}
+          <div className="w-full flex justify-center mr-32">
+            <div className="hidden md:flex items-center justify-between space-x-1 gap-x-4">
+              {navigation.map((item) => (
+                <NavbarItem
+                  key={item.to}
+                  isWhiteTheme={isWhiteTheme}
+                  to={item.to}
+                >
+                  {item.label}
+                </NavbarItem>
+              ))}
+            </div>
           </div>
-         </div>
         </div>
       </div>
     </nav>
