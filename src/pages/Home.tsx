77ad1fc0cd05257871ee46c056ca4ef91fd8bc27
Ajithkumar A <ajithkumar@pixzest.com>
2024-08-28 18:51:33 +0530

    Added Hero, Map, OurExpertise section to the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,18 @@
-import GridBackground from 'components/GridBackground'
-import Services from 'components/Services'
-import React from 'react'
+import GridBackground from "components/GridBackground";
+import Hero from "components/Hero";
+import Map from "components/Map";
+import OurExpertise from "components/OurExpertise";
+import Services from "components/Services";
+import React from "react";
 
 export default function Home() {
   return (
     <div>
-        <GridBackground/>
-        <Services/>
+      <GridBackground />
+      <Map />
+      <OurExpertise />
+      <Hero />
+      <Services />
     </div>
-  )
+  );
 }
